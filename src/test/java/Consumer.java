ff3a5cf19a49253c16045e3428f7a2a75c177674
zhanglong <zhanglong@bubugao.com>
2015-06-05 17:31:52 +0800

    提交泛化引用示例

--- src/test/java/Consumer.java
+++ src/test/java/Consumer.java
@@ -2,8 +2,6 @@ import java.io.IOException;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.study.dubbo.server.DemoService;
-
 /**
  * <p>Copyright: Copyright (c) 2014</p>
  * <p>Company: bubugao </p>
@@ -23,8 +21,8 @@ public class Consumer {
     public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext applicationContext  = new ClassPathXmlApplicationContext(new String[]{"/consumer.xml"});
         applicationContext.start();
-        DemoService bean = (DemoService) applicationContext.getBean("demoService",DemoService.class);
-        bean.sayHello();
+//        DemoService bean = (DemoService) applicationContext.getBean("demoService",DemoService.class);
+//        bean.sayHello();
         System.in.read();
     }
 }
--- src/test/java/GenericConsumer.java
+++ src/test/java/GenericConsumer.java
@@ -1,11 +1,17 @@
 import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
+import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.config.ApplicationConfig;
 import com.alibaba.dubbo.config.ReferenceConfig;
 import com.alibaba.dubbo.config.RegistryConfig;
 import com.alibaba.dubbo.config.utils.ReferenceConfigCache;
-import com.alibaba.dubbo.rpc.RpcContext;
-import com.alibaba.dubbo.rpc.RpcInvocation;
+import com.alibaba.dubbo.registry.Registry;
+import com.alibaba.dubbo.registry.support.AbstractRegistryFactory;
+import com.alibaba.dubbo.registry.zookeeper.ZookeeperRegistry;
 import com.alibaba.dubbo.rpc.service.GenericService;
 
 /**
@@ -30,22 +36,33 @@ public class GenericConsumer {
         application.setName("consumer-of-helloworld-app");
          
         // 连接注册中心配置
-        RegistryConfig registry = new RegistryConfig();
-        registry.setProtocol("zookeeper");
-        registry.setAddress("192.168.44.155:2181");
+        RegistryConfig registryConfig = new RegistryConfig();
+        registryConfig.setProtocol("zookeeper");
+        registryConfig.setAddress("192.168.44.155:2181");
      // 引用远程服务 
         ReferenceConfig<GenericService> reference = new ReferenceConfig<GenericService>(); // 该实例很重量，里面封装了所有与注册中心及服务提供方连接，请缓存
         reference.setApplication(application);
-        reference.setRegistry(registry);
+        reference.setRegistry(registryConfig);
         reference.setInterface("com.study.dubbo.server.DemoService"); // 弱类型接口名 
         reference.setGeneric(true); // 声明为泛化接口 
-        
         ReferenceConfigCache cache = ReferenceConfigCache.getCache();
         GenericService genericService = cache.get(reference);
 //        GenericService genericService = reference.get(); // 用com.alibaba.dubbo.rpc.service.GenericService可以替代所有接口引用 
-         
         // 基本类型以及Date,List,Map等不需要转换，直接调用 
-        Object result = genericService.$invoke("sayHello", null, null); 
+        Map<String, Object> person = new HashMap<String, Object>(); 
+        person.put("name", "xxx"); 
+        person.put("age", 111); 
+        Object result = genericService.$invoke("doSomething", new String[]{"com.study.dubbo.model.Person"}, new Object[]{person}); 
+        System.out.println(result);
+        Collection<Registry> registries = AbstractRegistryFactory.getRegistries();
+        for (Registry registry : registries) {
+            ZookeeperRegistry zookeeperRegistry=(ZookeeperRegistry) registry;
+            Set<URL> registered = zookeeperRegistry.getRegistered();
+            for (URL url : registered) {
+                System.out.println(url);
+            }
+        }
+        
          
        
     }
